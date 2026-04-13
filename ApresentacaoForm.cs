6fe6d3dde6156ca389302624fd541c3d13ccc4c2
LabInfo <LabInfo@TERREOA2M15>
2026-04-13 12:24:27 -0300

    Adicionar arquivos de projeto.

---
+++ ApresentacaoForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace POO
+{
+    internal class ApresentacaoForm: Form
+    {
+        //objeto label
+        Label titulo01 = new Label();
+        Label titulo02 = new Label();
+        Automovel obj01 = new Automovel();
+
+        //CONSTRUTOR
+        public ApresentacaoForm()
+        {
+            this.Text = "Apresentação";
+            this.Size = new Size(500, 500);
+            titulo01.Text = "Apresentação de elementos em classe!!";
+            titulo01.Font = new Font("Arial", 16);
+            titulo01.AutoSize = true; //ajustar no comprimento do texto
+            titulo01.Location = new Point(100, 100);
+            //add MARCA a objeto 
+            obj01.Marca = "Ford";
+            titulo02.Location = new Point(105, 105);
+            titulo02.AutoSize = true;
+            titulo02.Text = obj01.Marca;
+            //fazer o elemento exibir no form
+            //array de controls
+            Control[] elementos = { titulo01, titulo02 };
+            this.Controls.AddRange(elementos);
+            //this.Controls.Add(titulo02);
+            //this.Controls.Add(titulo01);
+        }
+    }
+}
---
+++ Automovel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    internal class Automovel
+    {
+        //visibilidade private, public, protected, internal
+        //public - pode ser acessado por qualquer classe
+        //private - só pode ser acessada pela própria classe
+        //protected - própria classe e derivadas
+        //internal - só pelo mesmo assembly
+
+        //construtor
+        //métodos de acesso
+        /*public void SetMarca(string x) 
+        { 
+            Marca = x;
+        }
+        public string GetMarca() 
+        {  
+            return Marca; 
+        } */
+        public string Marca { get; set; }  //get retorna - set atribui
+        public Automovel() 
+        { 
+            
+        }
+    }
+}
